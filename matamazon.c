3f6e1402412189030c043f7ac849361d542aa49c
davidcalard <58255839+davidcalard@users.noreply.github.com>
2019-11-29 12:07:03 +0200

    Update matamazon.c

--- matamazon.c
+++ matamazon.c
@@ -1,1 +1,13 @@
 
+static SetElement  copyInt(SetElement number) {
+    int *copy = malloc(sizeof(*copy));
+    if (copy != NULL) {
+        *copy = *(int *)number;
+    }
+    return copy;
+}
+
+static void freeInt(SetElement number) { free(number); }
+
+static int compareInts(SetElement lhs, SetElement rhs) {
+    return (*(int *)lhs) - (*(int *)rhs);
